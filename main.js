fa78b7ec168cccd14f3f68a363f046af8492c09b
zoglam <anzoor1998@gmail.com>
2020-03-26 19:36:20 +0300

    интерфейс + логика алгр.

--- main.js
+++ main.js
@@ -1,66 +1,116 @@
-﻿var listLeft = new Map([]);//Левый столбец
-var listRight = new Map([]);//Правый столбец
-var step = 0;
-function rule() {
-  console.clear();
+﻿document.addEventListener('DOMContentLoaded', saveEmptyRule);
 
-  listLeft.set(step, ruleLeft.value);//Добавление правил
-  listRight.set(step, ruleRight.value);
-  step++;
-
-  let ruleProgress = "Rules: <br />"
-
-  //Цикл отображения таблицы правил в конце
-  for (let j = 0; j < listLeft.size; j++) {
-    if (j == 0) console.log("\n\n\n\nRules:\n")
-    console.log((j + 1) + ":\t\t" + listLeft.get(j) + " → " + listRight.get(j));
-    ruleProgress += (j + 1) + ":&emsp;" + listLeft.get(j) + " → " + listRight.get(j) + "<br />";
-  }
-  document.getElementById('rule').innerHTML = ruleProgress;
-  return listLeft, listRight;
+function saveEmptyRule() {
+  count = 1
+  emptyRule = document.getElementsByClassName("rule")[0]
+  rule = emptyRule.outerHTML
+  document.getElementsByClassName("deleteRule")[0].remove()
 }
 
-function main() {
-  document.getElementById('error').style.display = "none";
+//Ограничения ввода данных в ячейки
+function validate(evt, id) {
+  var theEvent = evt || window.event;
+  var key = theEvent.keyCode || theEvent.which;
+  key = String.fromCharCode(key);
+  if (id == "word") {
+    var regex = /[A-z]|[0-9]|\+|\-|\*/;
+  } else if (id == "iterationLimit") {
+    var regex = /[0-9]/;
+  } else {
+    var regex = /[A-z]|[0-9]|\+|\-|\*|\$|\_/;
+  }
+  if (!regex.test(key)) {
+    theEvent.returnValue = false;
+    if (theEvent.preventDefault) theEvent.preventDefault();
+  }
+}
 
-  //Ввести слово
-  let input = word.value;
-  /*
-    ------------------------------------------------------------------------------------
-  */
-  let VARIABLE = " " + input;//добавление пробела перед словом
-  console.log("\n\n\n\nTarget: \t\t" + VARIABLE);//вывод в консоль условия
+function setIdForRules() {
+  let allRules = document.getElementsByClassName("rule")
+  for (let i = 0; i < allRules.length; i++) {
+    allRules[i].childNodes[1].innerText = "Rule " + (i + 1) + ":"
+  }
+  return allRules
+}
+
+function increaseId(direction) {
+  direction ? count++ : count--;
+  return rule
+}
+
+function insertAfter(parent, node, referenceNode) {
+  parent.insertBefore(node, referenceNode.nextSibling);
+}
+
+function deleteRule(currentRule) {
+  ruleID = currentRule.parentNode.parentNode.id
+  if (ruleID != 1)
+    if (ruleID == count)
+      increaseId(false)
+  currentRule.parentNode.parentNode.remove()
+}
+
+function addDown(currentRule) {
+  let prevElem = document.getElementById(currentRule.parentNode.parentNode.id)
+  let parentDiv = document.getElementById("rules")
+  let newRule = document.createElement('div')
+  newRule.innerHTML = increaseId(true)
+  newRule.id = count
+  insertAfter(parentDiv, newRule, prevElem)
+}
+
+function solve() {
+  let allRules = setIdForRules()
+
+  let word = document.getElementById("word").value
+  let VARIABLE = "_" + word;
   document.getElementById('target').innerHTML = "<b>Target:&emsp;" + VARIABLE + "</b>";
 
+  let listLeft = new Map([]); //Левый столбец
+  let listRight = new Map([]); //Правый столбец
+
+  let iterationLimit = +(document.getElementById("iterationLimit").value)
+  let errorMessage = "<b>Error: no end of cycle | max iterations = " + iterationLimit + "</b>"
   let progress = "";
 
-  //Цикл, использующий правила, для преобразований
-  for (let i = 0; ;) {
-    if (i == 2000 || (listLeft.get(i) == "" && listRight.get(i) == "")) {
-      console.log("Error: no end of cycle");
-      document.getElementById('error').innerHTML = "<b>Error: no end of cycle</b>";
+  for (let i = 0; i < allRules.length; i++) {
+    let key = allRules[i].childNodes[3].value
+    let value = allRules[i].childNodes[7].value
+    listLeft.set(i, key); //Добавление правил
+    listRight.set(i, value);
+  }
+
+  let j = 0
+  for (let i = 0;; j++) {
+    if (j >= iterationLimit || (listLeft.get(i) == "" && listRight.get(i) == "")) {
+      document.getElementById('error').innerHTML = errorMessage
       document.getElementById('error').style.display = "block";
       break;
+    } else {
+      document.getElementById('error').style.display = "none";
     }
+
+    if (VARIABLE.search("_") == -1) {
+      VARIABLE = "_" + VARIABLE
+    }
+
     if (VARIABLE.indexOf(listLeft.get(i)) == -1) {
       i++;
       continue;
-    }
-    else {
+    } else {
       VARIABLE = VARIABLE.replace(listLeft.get(i), listRight.get(i));
       //Проверка на основку программы
-      if (listRight.get(i) == "O") {
-        VARIABLE = VARIABLE.replace("O", " ");
-        console.log("Rtrn: " + (i + 1) + " " + "\t\t" + VARIABLE);
-        progress += "<b>Rtrn: " + (i + 1) + "&emsp;" + VARIABLE + "</b><br />";
+      if (listRight.get(i) == "$") {
+        VARIABLE = VARIABLE.replace("$", " ");
+        progress += "<b>Ans: " + (i + 1) + "&emsp;" + VARIABLE + "</b><br />";
         break;
       }
 
-      console.log("Rule: " + (i + 1) + " " + "\t\t" + VARIABLE);
       progress += "Rule: " + (i + 1) + " " + "&emsp;" + VARIABLE + "<br />";
       i = 0;
       continue;
     }
   }
   document.getElementById('progress').innerHTML = progress;
+
 }
